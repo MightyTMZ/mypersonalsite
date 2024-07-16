5125f4647c71479f39a13e6f3c69bbccdb979248
Tom Zhang <tommyawesome08@gmail.com>
2024-07-16 10:59:49 -0400

    projects admin ready

--- mypersonalsite/projects/admin.py
+++ mypersonalsite/projects/admin.py
@@ -1,2 +1,29 @@
 from django.contrib import admin
+from .models import *
 
+
+class PhotoInline(admin.StackedInline):
+    model = Photo
+    extra = 1
+
+
+class AchievementInline(admin.StackedInline):
+    model = Achievement
+    extra = 1
+
+
+class LinkInline(admin.StackedInline):
+    model = Link
+    extra = 1
+
+
+@admin.register(Project)
+class VentureAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'visible',
+    )
+    # you can think about adding a displayed field(s) of the number of images, achievements
+
+    inlines = [LinkInline, AchievementInline, PhotoInline]
