99e92c93af2ce097d8d08434236a32cfb710fc1e
Tom Zhang <tommyawesome08@gmail.com>
2024-07-16 10:57:00 -0400

    current_ventures admin ready

--- mypersonalsite/current_ventures/admin.py
+++ mypersonalsite/current_ventures/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class TestimonialInline(admin.StackedInline):
+    model = Testimonial
+    extra = 1
+
+
+class AchievementInline(admin.StackedInline):
+    model = Achievement
+    extra = 1
+
+
+@admin.register(Venture)
+class VentureAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'start_date',
+        'end_date',
+        'website'
+    )
+
+    list_editable = (
+        'title',
+        "description"
+        'website'
+    )
+
+    inlines = [TestimonialInline, AchievementInline]
