e582e7df77119f36894f54c6bf44d3cb0128bf63
Tom Zhang <tommyawesome08@gmail.com>
2024-07-16 10:34:53 -0400

    created the data model

---
+++ mypersonalsite/current_ventures/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Venture(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    website = models.URLField(max_length=2083, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Testimonial(models.Model):
+    venture = models.ForeignKey(Venture, on_delete=models.CASCADE, related_name='testimonials')
+    name = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    testimonial = models.TextField()
+
+    def __str__(self):
+        return f"{self.name} - {self.role}"
+
+
+class Achievement(models.Model):
+    venture = models.ForeignKey(Venture, on_delete=models.CASCADE, related_name='achievements')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
--- mypersonalsite/projects/models.py
+++ mypersonalsite/projects/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    visible = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to="media/")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="images")
+
+
+class Link(models.Model):
+
+    TYPES_OF_LINKS = [
+        ('GitHub', 'GitHub'),
+        ('Devpost', 'Devpost'),
+        ('Site', 'Site'),
+        ('Demo', 'Demo')
+    ]
+
+    link = models.CharField(max_length=2083)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='links')
+    type = models.CharField(max_length=255, choices=TYPES_OF_LINKS)
+
+    def __str__(self):
+        return self.link
+
+
+class Achievement(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    # the description field will appear when the user hovers over the achievement icon on the frontend
+    icon = models.ImageField(upload_to="media/icon")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    # will serve as a badge for all our projects
+
+    def __str__(self):
+        return self.title
+
+
+
