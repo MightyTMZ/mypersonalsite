dfb888f33d9eaf2e4cea0477e3f273eeff3e012b
Tom Zhang <tommyawesome08@gmail.com>
2024-07-17 12:50:40 -0400

    created skills serializer

---
+++ mypersonalsite/skills/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import *
+
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = [
+            'title', 'description', 'icon'
+        ]
