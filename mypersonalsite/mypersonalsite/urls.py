c1d82716787531072f9e03f948240196399f50b2
Tom Zhang <tommyawesome08@gmail.com>
2024-07-17 12:40:50 -0400

    projects app serializer created

--- mypersonalsite/mypersonalsite/urls.py
+++ mypersonalsite/mypersonalsite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('projects/', include('projects.urls')),
+    path('blog/', include('blog.urls')), # make sure you add a page on the frontend that consolidates all your articles
+    path('current-ventures/', include('current_ventures.urls'))
 ]
---
+++ mypersonalsite/projects/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import *
+
+
+class PhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Photo
+        fields = ['image']
+
+
+class LinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Link
+        fields = ["link", 'type']
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Achievement
+        fields = [
+            'title', 
+            'description', 
+            'icon', 
+        ]
+
+
+
+class ProjectSerializer(serializers.Serializer):
+    images = PhotoSerializer(many=True, read_only=True)
+    links = LinkSerializer(many=True, read_only=True)
+    achievements = AchievementSerializer(many=True, read_only=True, source='achievement_set')
+
+    class Meta:
+        model = Project
+        fields = ['title', 'description', 'visible', 'images', 'links', 'achievements']
